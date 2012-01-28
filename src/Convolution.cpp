51a657e5b3e37f1d1a78d52275f3de82946f1b46
Kevin Hutchins <possessedcow@gmail.com>
2012-01-28 00:23:34 -0800

    Removed extraneous debugging code

--- src/Convolution.cpp
+++ src/Convolution.cpp
@@ -20,11 +20,8 @@ BMP generateConvolutionBitmap(BMP originalImage, vector<vector<double>> &convolu
 				for(int c_x = 0; c_x < convolutionWidth; c_x++) {
 					int x = min(max(0,i_x+c_x-(c_x-1)/2),imageWidth-1);
 					int y = min(max(0,i_y+c_y-(c_y-1)/2),imageHeight-1);
+
 					RGBApixel tempPixel = originalImage.GetPixel(x,y);
-					if(tempPixel.Green > 0)
-					{
-						int x = 5;
-					}
 					newColor.r += tempPixel.Red * convolution[c_y][c_x];
 					newColor.g += tempPixel.Green * convolution[c_y][c_x];
 					newColor.b += tempPixel.Blue * convolution[c_y][c_x];
